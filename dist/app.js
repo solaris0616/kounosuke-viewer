cf3fa7171014d3968ae2e598551399ca4b434803
solaris0616 <solaris0616@gmail.com>
2019-07-30 17:22:14 +0900

    Initial commit: Hello world

---
+++ dist/app.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var _express = _interopRequireDefault(require("express"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var app = (0, _express["default"])();
+app.get('/', function (req, res) {
+  res.send("Hello world.");
+});
+app.listen(3000, function () {
+  console.log("App listening on port 3000!");
+});
