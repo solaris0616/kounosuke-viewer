0a51972ab0ce545d3555c293e5a42c4c12a5b56e
solaris0616 <solaris0616@gmail.com>
2019-07-30 18:04:26 +0900

    Scrape title using request-promise and cheerio.

--- dist/app.js
+++ dist/app.js
@@ -2,10 +2,25 @@
 
 var _express = _interopRequireDefault(require("express"));
 
+var _requestPromise = _interopRequireDefault(require("request-promise"));
+
+var _cheerio = _interopRequireDefault(require("cheerio"));
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 var app = (0, _express["default"])();
+var options = {
+  uri: 'https://www.panasonic.com/jp/corporate/history/founders-quotes.html',
+  transform: function transform(body) {
+    return _cheerio["default"].load(body);
+  }
+};
 app.get('/', function (req, res) {
+  (0, _requestPromise["default"])(options).then(function ($) {
+    console.log($('title').text());
+  })["catch"](function (error) {
+    console.error('Error:', error);
+  });
   res.send("Hello world.");
 });
 app.listen(3000, function () {
--- src/app.js
+++ src/app.js
@@ -1,8 +1,24 @@
 import express from "express";
+import rp from 'request-promise';
+import cheerio from 'cheerio';
 
 const app = express();
 
+const options = {
+    uri: 'https://www.panasonic.com/jp/corporate/history/founders-quotes.html',
+    transform: (body) => {
+        return cheerio.load(body);
+    }
+}
+
 app.get('/', (req, res) => {
+    rp(options)
+        .then(($) => {
+            console.log($('title').text());
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
     res.send("Hello world.");
 });
 
