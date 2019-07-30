5c877aa8ebffafa856d81cb785203435e05c7f22
solaris0616 <solaris0616@gmail.com>
2019-07-30 20:39:28 +0900

    Specify URI for request-promise based on JST..

--- dist/app.js
+++ dist/app.js
@@ -11,19 +11,31 @@ var _iconv = _interopRequireDefault(require("iconv"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 var app = (0, _express["default"])();
-var options = {
-  uri: 'https://www.panasonic.com/content/dam/panasonic/jp/corporate/history/founders-quotes/resource/DS0730.HTML',
-  encoding: null,
-  transform: function transform(body) {
-    var iconv = _iconv["default"].Iconv('SHIFT_JIS', 'UTF-8');
 
-    var utf8Body = iconv.convert(body).toString();
-    return _cheerio["default"].load(utf8Body, {
-      decodeEntities: false
-    });
-  }
+var createOptions = function createOptions() {
+  var jstOffsetMs = -9 * 60 * 60 * 1000;
+  var localOffsetMs = new Date().getTimezoneOffset() * 60 * 1000;
+  var todayJST = new Date(Date.now() - localOffsetMs + jstOffsetMs);
+  var month = ('00' + (todayJST.getMonth() + 1)).slice(-2);
+  var date = ('00' + todayJST.getDate()).slice(-2);
+  var filename = "https://www.panasonic.com/content/dam/panasonic/jp/corporate/history/founders-quotes/resource/DS".concat(month).concat(date, ".HTML");
+  var option = {
+    uri: filename,
+    encoding: null,
+    transform: function transform(body) {
+      var iconv = _iconv["default"].Iconv('SHIFT_JIS', 'UTF-8');
+
+      var utf8Body = iconv.convert(body).toString();
+      return _cheerio["default"].load(utf8Body, {
+        decodeEntities: false
+      });
+    }
+  };
+  return option;
 };
+
 app.get('/', function (req, res) {
+  var options = createOptions();
   (0, _requestPromise["default"])(options).then(function ($) {
     var header = $('h1').html();
     var content = $('p').html();
--- src/app.js
+++ src/app.js
@@ -5,24 +5,37 @@ import Iconv from 'iconv';
 
 const app = express();
 
-const options = {
-    uri: 'https://www.panasonic.com/content/dam/panasonic/jp/corporate/history/founders-quotes/resource/DS0730.HTML',
-    encoding: null,
-    transform: (body) => {
-        const iconv = Iconv.Iconv('SHIFT_JIS', 'UTF-8');
-        const utf8Body = iconv.convert(body).toString();
-        return cheerio.load(utf8Body, {decodeEntities: false});
-    }
+const createOptions = () => {
+    const jstOffsetMs = (-9) * 60 * 60 * 1000;
+    const localOffsetMs = (new Date().getTimezoneOffset()) * 60 * 1000;
+    const todayJST = new Date(Date.now() - localOffsetMs + jstOffsetMs);
+
+    const month = (('00' + (todayJST.getMonth() + 1)).slice(-2));
+    const date = (('00' + todayJST.getDate()).slice(-2));
+    const filename = `https://www.panasonic.com/content/dam/panasonic/jp/corporate/history/founders-quotes/resource/DS${month}${date}.HTML`
+
+    const option = {
+        uri: filename,
+        encoding: null,
+        transform: (body) => {
+            const iconv = Iconv.Iconv('SHIFT_JIS', 'UTF-8');
+            const utf8Body = iconv.convert(body).toString();
+            return cheerio.load(utf8Body, {decodeEntities: false});
+        }
+    };
+    return option;
 }
 
 app.get('/', (req, res) => {
+    const options = createOptions();
+
     rp(options)
         .then(($) => {
             const header = $('h1').html();
             const content = $('p').html();
             res.send({
-                title: header,
-                contents: content
+                header: header,
+                content: content
             });
         })
         .catch((error) => {
